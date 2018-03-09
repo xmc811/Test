9682f1a30677c1fb1e666635c4ce4c85983532f9
Mingchu Xu <mingchu.xu@alumni.bcm.edu>
2018-03-09 17:06:46 -0600

    Branching test

---
+++ bootstrap_simulation.R
@@ -0,0 +1,25 @@
+# This is a experimental version on Github branching
+
+a <- rnorm(10000, mean = 1, sd = 0.5)
+b <- rnorm(10000, mean = 3, sd = 1)
+c <- 2 ** a
+
+merged <- append(a, c)
+
+s1 <- sample(merged, 50, replace = FALSE)
+
+alpha <- 0.05
+
+iteration <- 100000
+
+est <- c()
+
+for (i in 1:iteration) {
+    
+    s2 <- sample(s1, length(s1), replace = TRUE)
+    est[i] <- sd(s2)
+    
+}
+
+quantile(est, c(alpha / 2, 1 - (alpha / 2)))
+
